252db7f4f368425ab55317ccd1cacdbeef24ae7f
Cristiano S Oliveira <cristiano.so@hotmail.com>
2020-08-05 22:35:58 -0300

    feat: finalize form

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,28 +2,46 @@ import React from "react";
 
 import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
+import Input from "../../components/Input";
+import Select from "../../components/Select";
 
 import "./styles.css";
+
+
 
 function TeacherList() {
   return (
     <div className="container" id="page-teacher-list">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
+          <Select 
+            name="subject" 
+            label="Matéria"
+            options={[
+              { value: 'Artes', label: 'Artes'},
+              { value: 'Ciências', label: 'Ciências'},
+              { value: 'Biologia', label: 'Biologia'},
+              { value: 'Física', label: 'Física'},
+              { value: 'Matemática', label: 'Matemárica'},
+              { value: 'Português', label: 'Português'}
 
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="subject" />
-          </div>
+            ]}
+          />
+          <Select 
+            name="week_day" 
+            label="Dia da semana"
+            options={[
+              { value: '0', label: 'Domingo'},
+              { value: '1', label: 'Segunda-feira'},
+              { value: '2', label: 'Terça-feira'},
+              { value: '3', label: 'Quarta-feira'},
+              { value: '4', label: 'Quinta-feira'},
+              { value: '5', label: 'Sexta-feira'},
+              { value: '6', label: 'Sabado'}
 
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="subject" />
-          </div>
+            ]}
+          />
+          <Input type="time" name="time" label="Hora"/>
         </form>
       </PageHeader>
 
